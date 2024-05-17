8c901eabc70781ce4f5a557749578a9aae96b663
Dhia BH <dhia@Dhias-MacBook-Pro.local>
2024-05-17 15:14:20 +0100

    feat: 🎸 WIP: setup project

---
+++ scripts/folders.sh
@@ -0,0 +1,17 @@
+cd ..
+cd src
+
+mkdir app
+mkdir assets
+mkdir services
+mkdir layout
+mkdir components
+mkdir screens
+mkdir translations
+mkdir context
+mkdir hooks
+mkdir mock
+mkdir types
+mkdir api
+mkdir routes
+mkdir utils
